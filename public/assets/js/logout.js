cb9c83085b3faf364d7bf58a53d41b8bc066f6ff
Don McLean <mclean@utoronto.ca>
2022-03-30 14:52:03 -0400

    boilerplate draft for logout.js

--- public/assets/js/logout.js
+++ public/assets/js/logout.js
@@ -1,2 +1,17 @@
 // placeholder for public/js/logout.js
 // to develop on branch develop feature/logout
+// provides boilerplate for logout form handling
+const logout = async () => {
+ const response = await fetch('/api/users/logout', {
+   method: 'POST',
+   headers: { 'Content-Type': 'application/json' },
+ });
+
+ if (response.ok) {
+   document.location.replace('/');
+ } else {
+   alert('Failed to log out.');
+ }
+};
+
+document.querySelector('#logout').addEventListener('click', logout);
