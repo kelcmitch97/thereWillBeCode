a6a008eba82bea956a9eff384b53b39df5aa4709
EvanAB14 <evan.a.boileau14@gmail.com>
2022-03-31 02:42:27 -0400

    added simple animation on viewing id location, example animation, comments available

--- public/assets/js/animations.js
+++ public/assets/js/animations.js
@@ -1,1 +1,32 @@
 // Animations go here
+
+var observer = new IntersectionObserver(function(entries) {
+	if(entries[0].isIntersecting === true){
+
+        // Checking observer call below if it's visible
+
+		console.log('Animations can be added here')
+
+        // Checking within entries to find the ID
+
+        console.log(entries[0].target.id)
+
+        // can add animation by ID name visible on screen (other targets available IE className)
+
+        console.log(entries[0].target.className)
+
+        if (entries[0].target.id === "location"){
+
+            console.log('should work?')
+
+            // call       //target     //duration      //animations to take place over duration         //power
+
+            TweenMax.to($('#location'), 0.7, { backgroundColor: 'red', x: 100, transformOrigin:"50% 50%", ease:Power1.easeInOut });
+
+        }
+
+    }
+
+}, { threshold: [1] });
+
+observer.observe(document.getElementById("location"));
