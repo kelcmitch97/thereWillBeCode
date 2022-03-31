3964ef17ce0072cc62de0e736aff335ce66c783b
EvanAB14 <evan.a.boileau14@gmail.com>
2022-03-31 17:24:12 -0400

    basic animations added to page load, basic hover on swiper directions, for loop displaying event cards in order

--- public/assets/js/animations.js
+++ public/assets/js/animations.js
@@ -38,11 +38,28 @@ var observer = new IntersectionObserver(function(entries) {
 
         }
 
+        if (entries[0].target.className === "hero"){
+            // console.log('hero visible')
+
+            // Repeating animation on page load
+
+            TweenMax.to($('.hero h2'), 0, { opacity: 0, x: -30, y: -30, transformOrigin:"50% 50%", ease:Power1.easeInOut, repeat: 0 });
+            TweenMax.to($('.hero h2'), 1.0, { opacity: 1, x: 0, y: 0, transformOrigin:"50% 50%", ease:Power1.easeInOut, repeat: 0 });
+
+            TweenMax.to($('.hero p'), 0, { opacity: 0, y: 20, x: 20, transformOrigin:"50% 50%", ease:Power1.easeInOut, repeat: 0 });
+            TweenMax.to($('.hero p'), 0.5, { delay: 0.5, opacity: 1, y: 0, x: 0, transformOrigin:"50% 50%", ease:Power1.easeInOut, repeat: 0 });
+
+            TweenMax.to($('.hero button'), 0, { opacity: 0, y: 50, transformOrigin:"50% 50%", ease:Power1.easeInOut, repeat: 0 });
+            TweenMax.to($('.hero button'), 1.0, { delay: 0.5, opacity: 1, y: 0, transformOrigin:"50% 50%", ease:Power1.easeInOut, repeat: 0 });
+        }
+
+
     }
 
 }, { threshold: [0] });
 
 observer.observe(document.querySelector(".circle"));
+observer.observe(document.querySelector(".hero"));
 
 // Swiper
 
@@ -59,3 +76,75 @@ let swiper = new Swiper(".event_container", {
     },
   
   });
+
+  // On load animations
+
+TweenMax.to($('.hero h2'), 0, { opacity: 0, x: -30, y: -30, transformOrigin:"50% 50%", ease:Power1.easeInOut, repeat: 0 });
+TweenMax.to($('.hero h2'), 1.0, { opacity: 1, x: 0, y: 0, transformOrigin:"50% 50%", ease:Power1.easeInOut, repeat: 0 });
+
+TweenMax.to($('.hero p'), 0, { opacity: 0, y: 20, x: 20, transformOrigin:"50% 50%", ease:Power1.easeInOut, repeat: 0 });
+TweenMax.to($('.hero p'), 0.5, { delay: 0.5, opacity: 1, y: 0, x: 0, transformOrigin:"50% 50%", ease:Power1.easeInOut, repeat: 0 });
+
+TweenMax.to($('.hero button'), 0, { opacity: 0, y: 50, transformOrigin:"50% 50%", ease:Power1.easeInOut, repeat: 0 });
+TweenMax.to($('.hero button'), 1.0, { delay: 0.5, opacity: 1, y: 0, transformOrigin:"50% 50%", ease:Power1.easeInOut, repeat: 0 });
+
+// jQuery Constants
+
+const eventCard = $('.event')
+const swiperNext = $('.swiper-button-next');
+const swiperPrev = $('.swiper-button-prev');
+
+// For loop displaying event cards
+
+for (let i = 0; i < eventCard.length; i++) {
+
+        TweenMax.to(eventCard[i], 0, { opacity: 0, transformOrigin:"50% 50%", ease:Power1.easeInOut, repeat: 0 });
+        TweenMax.to(eventCard[i], 1.0, { delay: i/3, opacity: 1, y: 0, transformOrigin:"50% 50%", ease:Power1.easeInOut, repeat: 0 });
+    
+}
+
+// Hover Functions
+
+// Swiper Right
+
+$(document).ready(function() {
+
+    swiperNext.hover(over, out);
+
+        var icon = swiperNext.find('i');
+
+        function over(){
+
+            TweenMax.to(icon, 0.5, { x: 30, transformOrigin:"50% 0%", ease:Power1.easeInOut, yoyo: true, repeat: -1 });
+
+        }
+
+        function out(){
+
+            TweenMax.to(icon, 0.5, { x: 0, transformOrigin:"50% 0%", ease:Power1.easeInOut });
+
+        }
+
+});
+
+// Swiper Left
+
+$(document).ready(function() {
+
+    swiperPrev.hover(over, out);
+
+        var icon = swiperPrev.find('i');
+
+        function over(){
+
+            TweenMax.to(icon, 0.5, { x: -30, transformOrigin:"50% 0%", ease:Power1.easeInOut, yoyo: true, repeat: -1 });
+
+        }
+
+        function out(){
+
+            TweenMax.to(icon, 0.5, { x: 0, transformOrigin:"50% 0%", ease:Power1.easeInOut });
+
+        }
+
+});
