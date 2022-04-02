75e747b41071c68170272d71ceedc323646dcd7a
kelcmitch97 <kelciemitchelmore@gmail.com>
2022-04-02 13:26:04 -0230

    Add nav bar to events page

--- public/assets/js/functions.js
+++ public/assets/js/functions.js
@@ -1,3 +1,17 @@
+// NavBar
+
+function openNav() {
+    document.getElementById("sideNavBar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+  };
+  
+  function closeNav() {
+    document.getElementById("sideNavBar").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+    document.body.style.backgroundColor = "white";
+  };
+
 // API calls
 
 const usersAPI = "./api/users";
