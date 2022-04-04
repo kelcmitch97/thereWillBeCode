b5684c721114b319d9b7787ab5e6bb230bf839d3
Don McLean <mclean@utoronto.ca>
2022-04-04 14:26:00 -0400

    provides ids for generic venues, will coordinate with locations

---
+++ seeds/venue-seeds.js
@@ -0,0 +1,36 @@
+const { Venue } = require('../models');
+
+const venueData = [
+    {
+      id: 100,
+      venue: outdoor-rink
+    },
+    {
+     id: 101,
+     venue: indoor-arena
+   },
+   {
+    id: 200,
+    venue: baseball-diamond
+   },
+   {
+    id: 300,
+    venue: basketball-court
+  },
+  {
+   id: 301,
+   venue: outdoor-drypad
+  },
+  {
+   id: 400,
+   venue: soccer-field
+  },
+  {
+   id: 401,
+   venue: playing-field
+  },
+  {
+    id: 500,
+    venue: tennis-court
+  }
+ ]
