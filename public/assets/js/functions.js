3797d598de37d3c761cd365eb499aaeb2ea67f50
kelcmitch97 <kelciemitchelmore@gmail.com>
2022-04-04 21:45:12 -0230

    navbar issue

--- public/assets/js/functions.js
+++ public/assets/js/functions.js
@@ -1,3 +1,17 @@
+// Nav Bar
+
+function openNav() {
+    document.getElementById("sideNavBar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+  };
+  
+  function closeNav() {
+    document.getElementById("sideNavBar").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+    document.body.style.backgroundColor = "white";
+  };
+
 // API calls
 
 const usersAPI = "./api/users";
@@ -72,14 +86,3 @@ const getLocations = async () => {
 }
 
 
-function openNav() {
-    document.getElementById("sideNavBar").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
-  };
-  
-  function closeNav() {
-    document.getElementById("sideNavBar").style.width = "0";
-    document.getElementById("main").style.marginLeft= "0";
-    document.body.style.backgroundColor = "white";
-  };
