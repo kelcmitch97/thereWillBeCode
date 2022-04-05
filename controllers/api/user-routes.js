5d935a747bfc078a8aeb949137f142ce95ecd5ee
EvanAB14 <evan.a.boileau14@gmail.com>
2022-04-05 13:13:23 -0400

    user now logs in with session

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { User } = require('../../models');
+const withAuth = require('../../utils/auth');
 
 // get all users
 router.route('/')
@@ -83,124 +84,33 @@ router.route('/:id')
     });
 });
 
-// router.post('/', (req, res) => {
-
-//   User.create({
-//     username: req.body.username,
-//     email: req.body.email,
-//     password: req.body.password
-
-
-//     // NEEDS EXTRA INFOMATION ADDED EG. DESCRIPTION, SPORTS, ETC
-
-
-//   })
-//   .then(userData => {
-
-//     req.session.save(() => {
-
-//         // Logs In here
-
-//       req.session.user_id = userData.id;
-//       req.session.username = userData.username;
-//       req.session.loggedIn = true;
-
-//       res.json(userData);
-//     });
-//   })
-//     .catch(err => {
-//       console.log(err);
-//       res.status(500).json(err);
-//     });
-// });
-
-// router.post('/login', (req, res) => {
-//   User.findOne({
-//     where: {
-//       email: req.body.email
-//     }
-//   }).then(userData => {
-//     if (!userData) {
-//       res.status(400).json({ message: 'No user with that email address!' });
-//       return;
-//     }
-
-//     // Password Check
-
-//     const validPassword = userData.checkPassword(req.body.password);
-
-//     if (!validPassword) {
-//       res.status(400).json({ message: 'Incorrect password!' });
-//       return;
-//     }
-
-//     req.session.save(() => {
-
-//       // Session stuff
-
-//       req.session.user_id = userData.id;
-//       req.session.username = userData.username;
-//       req.session.loggedIn = true;
-
-//       res.json({ user: userData, message: 'You are now logged in!' });
-//     });
-//   });
-// });
-
-// router.put('/:id', (req, res) => {
-
-
-//   // pass in req.body instead to only update what's passed through
-
-//   User.update(req.body, {
-//     individualHooks: true,
-//     where: {
-//       id: req.params.id
-//     }
-//   })
-//     .then(userData => {
-//       if (!userData[0]) {
-//         res.status(404).json({ message: 'No user found with this id' });
-//         return;
-//       }
-//       res.json(userData);
-//     })
-//     .catch(err => {
-//       console.log(err);
-//       res.status(500).json(err);
-//     });
-// });
-
-// router.delete('/:id', (req, res) => {
-//   User.destroy({
-//     where: {
-//       id: req.params.id
-//     }
-//   })
-//     .then(userData => {
-//       if (!userData) {
-//         res.status(404).json({ message: 'No user found with this id' });
-//         return;
-//       }
-//       res.json(userData);
-//     })
-//     .catch(err => {
-//       console.log(err);
-//       res.status(500).json(err);
-//     });
-// });
-
-// router.post('/logout', (req, res) => {
-
-//   if (req.session.loggedIn) {
-//     req.session.destroy(() => {
-//       res.status(204).end();
-//     });
-//   }
-//   else {
-//     res.status(404).end();
-//   }
-
-// });
+router.post('/login', (req, res) => {
+    User.findOne({
+      where: {
+        username: req.body.username
+      }
+    }).then(dbUserData => {
+      if (!dbUserData) {
+        res.status(400).json({ message: 'No user with that username...' });
+        return;
+      }
+  
+      const validPassword = dbUserData.checkPassword(req.body.password);
+  
+      if (!validPassword) {
+        res.status(400).json({ message: 'Incorrect password!' });
+        return;
+      }
+  
+      req.session.save(() => {
+        
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.loggedIn = true;
+  
+        res.json({ user: dbUserData, message: 'You are now logged in!' });
+      });
+    });
+  });
 
 module.exports = router;
