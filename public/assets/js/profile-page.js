3e2c53a13b9a339243f1aaef8aaaf682d094421b
EvanAB14 <evan.a.boileau14@gmail.com>
2022-04-06 10:21:39 -0400

    front end for looping for sports on profile page

--- public/assets/js/profile-page.js
+++ public/assets/js/profile-page.js
@@ -15,3 +15,45 @@ $('#created_tab').on('click', function(){
     TweenMax.to($('#events_participating'), 0, { display: 'none' });
 
 });
+
+// USERS FETCH
+
+const usersAPI = "/api/users";
+const userText = $('.profile_info h3').text();
+
+var userData;
+
+const getUsers = async () => {
+
+    await fetch(usersAPI)
+    .then(res => res.json())
+
+    .then(function(data){
+
+        userData = data;
+
+        // Check for user
+        
+        for (let i = 0; i < userData.length; i++) {
+            
+            if (userText === userData[i].username){
+
+                var sportArray = userData[i].sports.split(',');
+
+                for (let i = 0; i < sportArray.length; i++) {
+                    
+                    $('.profile_sports').append(`
+                        <p>${sportArray[i]}</p>
+                    `)
+                    
+                }
+
+            }
+            
+        }
+
+    });
+
+}
+
+getUsers();
