7e769c7b04d0b1fe994609e639d445118e2abb5b
NicolasRojas-CENG <55503463+NicolasRojas-CENG@users.noreply.github.com>
2022-04-09 13:26:37 -0400

    Added JawsDB, but need someone to test the connection.

--- server.js
+++ server.js
@@ -26,6 +26,17 @@ const sess = {
   })
 };
 
+require('dotenv').config();
+
+// create connection to our db
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    });
+
 app.use(session(sess));
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
