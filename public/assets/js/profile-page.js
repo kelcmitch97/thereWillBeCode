bf0e4c9bc25aefd079c09ceb02be9e298077f294
EvanAB14 <evan.a.boileau14@gmail.com>
2022-04-10 15:56:36 -0400

    added edit user functionality

--- public/assets/js/profile-page.js
+++ public/assets/js/profile-page.js
@@ -74,11 +74,18 @@ $('#profile_edit_link').on('click', function(){
     var profileSports = $('.profile_sports');
     var profileSportsInputs = $('.profile_sports_inputs');
 
+    var profileNameInput = $('#profile_edit_username');
+    var profileDescInput = $('#profile_edit_description');
+
     TweenMax.to(profileName, 0, { display: 'none' });
     TweenMax.to(profileDescription, 0, { display: 'none' });
     TweenMax.to(profileSports, 0, { display: 'none' });
 
     TweenMax.to(profileSportsInputs, 0, { display: 'flex' });
+    TweenMax.to(profileDescInput, 0, { display: 'flex' });
+    TweenMax.to(profileNameInput, 0, { display: 'flex' });
+
+    var sportArray = [];
 
     $('label').on('click', function(){
 
@@ -90,11 +97,15 @@ $('#profile_edit_link').on('click', function(){
 
                 $('#sport-1').removeAttr('checked');
                 TweenMax.to($(this), 0, { backgroundColor: '#39a6b2' });
+                sportArray = sportArray.filter(filterThis => filterThis !== $('#sport-1').val());
+                
 
             } else {
 
             $('#sport-1').attr('checked', 'true');
             TweenMax.to($(this), 0, { backgroundColor: '#024e76' });
+            sportArray.push($('#sport-1').val());
+            
 
             }
 
@@ -106,16 +117,23 @@ $('#profile_edit_link').on('click', function(){
 
                 $('#sport-2').removeAttr('checked');
                 TweenMax.to($(this), 0, { backgroundColor: '#39a6b2' });
+                sportArray = sportArray.filter(filterThis => filterThis !== $('#sport-2').val());
+                
+
+                return sportArray;
 
             } else {
 
             $('#sport-2').attr('checked', 'true');
             TweenMax.to($(this), 0, { backgroundColor: '#024e76' });
+            sportArray.push($('#sport-2').val());
+            
+
+            return sportArray;
 
             }
-            
-        }
 
+        }
 
         if (labelID === 'input-sport-3'){
 
@@ -123,16 +141,23 @@ $('#profile_edit_link').on('click', function(){
 
                 $('#sport-3').removeAttr('checked');
                 TweenMax.to($(this), 0, { backgroundColor: '#39a6b2' });
+                sportArray = sportArray.filter(filterThis => filterThis !== $('#sport-3').val());
+                
+
+                return sportArray;
 
             } else {
 
             $('#sport-3').attr('checked', 'true');
             TweenMax.to($(this), 0, { backgroundColor: '#024e76' });
+            sportArray.push($('#sport-3').val());
+            
+
+            return sportArray;
 
             }
-            
-        }
 
+        }
 
         if (labelID === 'input-sport-4'){
 
@@ -140,16 +165,23 @@ $('#profile_edit_link').on('click', function(){
 
                 $('#sport-4').removeAttr('checked');
                 TweenMax.to($(this), 0, { backgroundColor: '#39a6b2' });
+                sportArray = sportArray.filter(filterThis => filterThis !== $('#sport-4').val());
+                
+
+                return sportArray;
 
             } else {
 
             $('#sport-4').attr('checked', 'true');
             TweenMax.to($(this), 0, { backgroundColor: '#024e76' });
+            sportArray.push($('#sport-4').val());
+            
+
+            return sportArray;
 
             }
-            
-        }
 
+        }
 
         if (labelID === 'input-sport-5'){
 
@@ -157,19 +189,77 @@ $('#profile_edit_link').on('click', function(){
 
                 $('#sport-5').removeAttr('checked');
                 TweenMax.to($(this), 0, { backgroundColor: '#39a6b2' });
+                sportArray = sportArray.filter(filterThis => filterThis !== $('#sport-5').val());
+                
+
+                return sportArray;
 
             } else {
 
             $('#sport-5').attr('checked', 'true');
             TweenMax.to($(this), 0, { backgroundColor: '#024e76' });
+            sportArray.push($('#sport-5').val());
+            
+
+            return sportArray;
 
             }
-            
+
         }
 
     })
 
+    $('#profile_save_link').on('click', function(){
+
+        async function saveProfile() {
+
+            if (sportArray.length === 0){
+                sports = '';
+            } else {
+                const sportsToString = sportArray.toString();
+                sports = sportsToString.replace(',', ', ');
+            }
+
+            const user_id = $('#profile_info_id').attr('value');
+            const username = $('#profile_edit_username').val();
+            const descriptionValue = $('#profile_edit_description').val();
+
+            if (descriptionValue.length === 0){
+
+                description = $('#profile_edit_description').attr('placeholder');
+
+            } else {
+
+                description = descriptionValue;
+
+            }
+            
+            const response = await fetch(`/api/users/${user_id}`, {
+                method: 'PUT',
+                body: JSON.stringify({
+                    sports,
+                    username,
+                    description
+                }),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+            if (response.ok) {
+                document.location.replace('/profile');
+            } else {
+                alert(response.statusText);
+            }
+        
+        };
+
+        saveProfile();
+
+    })
+    
 });
+
+
 
 // USERS FETCH
 
