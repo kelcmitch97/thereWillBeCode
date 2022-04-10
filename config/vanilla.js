78c5d86e3e277b7c92afadf11382f22ad80fa3b7
NicolasRojas-CENG <55503463+NicolasRojas-CENG@users.noreply.github.com>
2022-04-09 23:21:03 -0400

    This code should allow us to deploy to Heroku. Also added a session timer to log out the user after inactivity.

--- config/vanilla.js
+++ config/vanilla.js
@@ -1,21 +1,21 @@
-// Connect to database
-const mysql = require('mysql2');
+// // Connect to database
+// const mysql = require('mysql2');
 
-const dbHostname = process.env.DB_HOSTNAME;
-const dbUsername = process.env.DB_USER;
-const dbPassword = process.env.DB_PASSWORD;
+// const dbHostname = process.env.DB_HOSTNAME;
+// const dbUsername = process.env.DB_USER;
+// const dbPassword = process.env.DB_PASSWORD;
 
-const db = mysql.createConnection(
-    {
-      host: dbHostname,
-      // USE YOUR MySQL username
-      user: dbUsername,
-      // USE YOUR MySQL password
-      password: dbPassword,
-      // Database Name
-      database: 'pick_up_sports'
-    },
-    console.log('Connected to pick_up_sports database')
-  );
+// const db = mysql.createConnection(
+//     {
+//       host: dbHostname,
+//       // USE YOUR MySQL username
+//       user: dbUsername,
+//       // USE YOUR MySQL password
+//       password: dbPassword,
+//       // Database Name
+//       database: 'pick_up_sports'
+//     },
+//     console.log('Connected to pick_up_sports database')
+//   );
 
-module.exports = db;
+// module.exports = db;
